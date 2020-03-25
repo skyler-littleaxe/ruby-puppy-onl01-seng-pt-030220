57861932657f8f5a90d4ca31b5ec4a6edc754727
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-25 01:21:56 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+class Dog
+  attr_accessor :name
+  
+  @@all = []
+  @@names = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+    @@names << name
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+ def self.print_all
+  puts  @@
+  end
+  
+  
+end
